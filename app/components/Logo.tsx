7891484cd62d539575394ad1a5481271bd19b614
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-09-26 20:34:05 +0100

    Create NavBar component, rendered in root Layout

---
+++ app/components/Logo.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import Image from "next/image";
+
+import LogoImage from "./logo-green-72.png";
+
+const Logo = () => {
+  const router = useRouter();
+
+  return (
+    <Image
+      className="rounded-xl mr-auto cursor-pointer"
+      src={LogoImage}
+      alt="Ninja logo"
+      width="50"
+      // width="85"
+      quality={100}
+      placeholder="blur"
+      onClick={() => router.push("/")}
+    />
+  );
+};
+export default Logo;
---
+++ app/components/NavBar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+import Logo from "../components/Logo";
+
+const NavBar = () => {
+  return (
+    <nav className="relative w-full flex items-center gap-5 md:gap-10 mx-auto p-5 z-10">
+      <Logo />
+      <Link href="/" className="text-gray-500 hover:text-black">
+        ART
+      </Link>
+      <Link href="/about" className="text-gray-500 hover:text-black">
+        ABOUT
+      </Link>
+      <Link href="/login" className="text-gray-500 hover:text-black pr-5">
+        LOG IN
+      </Link>
+    </nav>
+  );
+};
+export default NavBar;

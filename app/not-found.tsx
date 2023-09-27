7590903353bf3c81cc22d5c47a3aa994079cb681
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-09-27 13:14:24 +0100

    Create not-found.tsx pg to handle not found errors

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full max-w-7xl flex flex-col flex-grow justify-center items-center mx-auto p-10">
+      <h1>The requested page does not exist!</h1>
+    </div>
+  );
+};
+export default NotFound;

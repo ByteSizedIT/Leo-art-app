780fa7cad11c82afc4ba518332fc1f9504c6a7c7
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-09-29 18:21:16 +0100

    Invoke signInWithEmailAndPassword function... ...in logIn function exported from UseLogIn hook
    - import useRouter and push to "/" on successful signup
    - handle errors and update error and isPending states

--- app/_hooks/useLogIn.ts
+++ app/_hooks/useLogIn.ts
@@ -1,17 +1,52 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
+import { firebaseAuth } from "../../firebase/config";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
+
+import useAuthContext from "./useAuthContext";
 
 const useLogIn = () => {
+  const [error, setError] = useState<string | null>(null);
   const [isPending, setIsPending] = useState(false);
-  const [error, setError] = useState(null);
+
+  const { authDispatch } = useAuthContext();
+
+  const router = useRouter();
 
   async function logIn(email: string, password: string) {
     console.log("logging in");
     console.log({ email, password });
 
     try {
-    } catch (err) {}
+      // sign in user with firebaseAuth
+      const response = await signInWithEmailAndPassword(
+        firebaseAuth,
+        email,
+        password
+      );
+
+      if (!response) throw new Error("Could not log in!");
+
+      console.log("User signed up as ", response.user);
+
+      // dispatch login action to update local user authState
+      authDispatch({ type: "LOG_IN", payload: response.user });
+
+      //redirect to homepage
+      router.push("/");
+    } catch (err) {
+      let message;
+      if (err instanceof Error) message = err.message;
+      else message = String(error);
+      console.log(message);
+      // send error to logging service such as Sentry
+
+      setError(message);
+      setIsPending(false);
+    }
   }
 
   return { isPending, error, logIn };

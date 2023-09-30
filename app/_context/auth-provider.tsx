4faae4300eb6b628f11a7c919a22ace9de374594
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-09-30 10:17:59 +0100

    Check authState on pg refresh (onAuthStateChange):
    - Invoke firebase onAuthStateChanged observer on mount, confirming whether a user session persists, and dispatch user details to reflect in AuthContext state.
    
    - console.log of authState in auth-provider.tsx demonstrates working in client when initial triggering of onAuthStateChange observer's callback function causes update of authState and therefore refresh of AuthContextProvider function component
    
    - TODO: resolve issues that, on page refresh while logged on to a firebase account, the initial wrong state of null causes login button to flash before onAuthStateChange causes dispatch function to update authState

--- app/_context/auth-provider.tsx
+++ app/_context/auth-provider.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { createContext, useReducer } from "react";
-
 // https://vercel.com/guides/react-context-state-management-nextjs#using-context-in-client-components
 
-const initialAuthState = { user: null };
+import { createContext, useEffect, useReducer } from "react";
+
+import { firebaseAuth } from "@/firebase/config";
+
+const initialAuthState = { user: null, authIsReady: false };
 
 export const AuthContext = createContext<{
   authState: AuthState;
@@ -16,6 +18,7 @@ export const AuthContext = createContext<{
 
 interface AuthState {
   user: {} | null;
+  authIsReady: boolean;
 }
 
 interface Action {
@@ -25,6 +28,8 @@ interface Action {
 
 export function authReducer(prevState: AuthState, action: Action) {
   switch (action.type) {
+    case "AUTH_IS_READY":
+      return { ...prevState, user: action.payload, authIsReady: true };
     case "LOG_IN":
       return { ...prevState, user: action.payload };
     case "LOG_OUT":
@@ -38,6 +43,15 @@ const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
   const [authState, authDispatch] = useReducer(authReducer, initialAuthState);
 
   console.log("AuthContext state: ", authState);
+
+  // Use Effect to overcome all components & AuthContext being recreated on browser refresh, and therefore user still logged on firebase not being reflected in local state as authState only gets otherwise updated by signUp, loginIn and logOut methods
+  useEffect(() => {
+    // onAuthStateChange observer - Effectively sets listener that fires cback function to dispatch every time there is a user state change on firebase. H/e it returns an unsubscribe function that can be invoked immediately after the first time it fires so that it only happens once on mount
+    const unsubscribe = firebaseAuth.onAuthStateChanged((userSessionCookie) => {
+      authDispatch({ type: "AUTH_IS_READY", payload: userSessionCookie });
+      unsubscribe();
+    });
+  }, []);
 
   return (
     <AuthContext.Provider value={{ authState, authDispatch }}>

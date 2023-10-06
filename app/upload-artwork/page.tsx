61243f7c41553cfe13a6c1d2a427bbb6107146ac
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-10-06 18:35:44 +0100

    Add placeholder for upload-artwork page

---
+++ app/upload-artwork/page.tsx
@@ -0,0 +1,8 @@
+const UploadArtwork = () => {
+  return (
+    <div className="flex flex-col flex-grow max-w-4xl justify-center items-center text-center mx-auto p-5">
+      <h1>UPLOAD NEW ARTWORK</h1>
+    </div>
+  );
+};
+export default UploadArtwork;

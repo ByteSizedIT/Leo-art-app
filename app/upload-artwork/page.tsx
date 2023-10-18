71282cc5425084908b1f7b46b31d3968de628a99
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2023-10-18 22:49:35 +0100

    Improve mobile responsive styling of uploadForm/page

--- app/upload-artwork/page.tsx
+++ app/upload-artwork/page.tsx
@@ -10,7 +10,7 @@ const UploadArtwork = async () => {
   const allArtWork: ArtWorkDownload[] = await getAllArtWork();
 
   return (
-    <div className="w-full flex flex-col justify-center items-center text-center mx-auto p-5">
+    <div className="w-full flex-grow flex flex-col justify-center items-center text-center mx-auto p-5">
       <h1>UPLOAD NEW ARTWORK</h1>
       <UploadForm allArtWork={allArtWork} />
     </div>
